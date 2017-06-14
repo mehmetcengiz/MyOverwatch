0f1ae86bf956c661c65a8e0156431330c9fa041b
Mehmet Cengiz <m.cengiz4716@gmail.com>
2017-06-14 17:39:53 +0300

    primary projectile created for soldier76

---
+++ Source/MyOverwatch/Projectiles/Soldier76PrimaryProjectile.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "Soldier76PrimaryProjectile.generated.h"
+
+UCLASS()
+class MYOVERWATCH_API ASoldier76PrimaryProjectile : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASoldier76PrimaryProjectile();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	
+	
+};
